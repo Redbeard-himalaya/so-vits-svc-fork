8293ae5a490e5d185f5ddcb46a929b0529d061db
redbeard <developer.bannon@gmail.com>
2023-08-10 16:19:12 +1200

    fix initial seg of preprocess_split.py
    Signed-off-by: redbeard <developer.bannon@gmail.com>

--- src/so_vits_svc_fork/preprocessing/preprocess_split.py
+++ src/so_vits_svc_fork/preprocessing/preprocess_split.py
@@ -1,15 +1,75 @@
 from __future__ import annotations
 
+import math
 from logging import getLogger
 from pathlib import Path
 
 import librosa
+import numpy
 import soundfile as sf
 from joblib import Parallel, delayed
 from tqdm import tqdm
 from tqdm_joblib import tqdm_joblib
 
 LOG = getLogger(__name__)
+
+class AudioSegment:
+    def __init__(self, start: numpy.int64, end: numpy.int64, sample_rate: int):
+        self.start = start
+        self.end = end
+        self.sample_rate = sample_rate
+
+    def __add__(self, other):
+        return AudioSegment(min([self.start, other.start]),
+                            max([self.end, other.end]),
+                            self.sample_rate)
+
+    def len(self):
+        return float(self.end - self.start) / self.sample_rate
+
+    def save(self, audio: numpy.ndarray, output_dir: Path, file_prefix: str):
+        audio_seg = audio[self.start:self.end]
+        start_t = float(self.start) / self.sample_rate
+        end_t = float(self.end) / self.sample_rate
+        file_path = output_dir / f"{file_prefix}_{start_t:.3f}_{end_t:.3f}.wav"
+        sf.write(file_path, audio_seg, self.sample_rate)
+
+
+def _process_one_ori(
+    input_path: Path,
+    output_dir: Path,
+    sr: int,
+    *,
+    top_db: int = 30,
+    frame_seconds: float = 0.5,
+    hop_seconds: float = 0.1,
+):
+    try:
+        audio, sr = librosa.load(input_path, sr=sr, mono=True)
+    except Exception as e:
+        LOG.warning(f"Failed to read {input_path}: {e}")
+        return
+    intervals = librosa.effects.split(
+        audio,
+        top_db=top_db,
+        frame_length=int(sr * frame_seconds),
+        hop_length=int(sr * hop_seconds),
+    )
+    output_dir.mkdir(parents=True, exist_ok=True)
+    dur_t_sum = 0.0
+    for start, end in tqdm(intervals, desc=f"Writing {input_path}"):
+        audio_cut = audio[start:end]
+        start_t = float(start) / sr
+        end_t = float(end) / sr
+        dur_t = end_t - start_t
+        if 5.0 <= dur_t and dur_t <= 15.0:
+            sf.write(
+                (output_dir / f"{input_path.stem}_{start_t:.3f}_{end_t:.3f}.wav"),
+                audio_cut,
+                sr,
+            )
+            dur_t_sum += dur_t
+    return dur_t_sum
 
 
 def _process_one(
@@ -34,18 +94,23 @@ def _process_one(
         hop_length=int(sr * hop_seconds),
     )
     output_dir.mkdir(parents=True, exist_ok=True)
+    dur_t_sum = 0.0
+    composed_audio_seg = AudioSegment(start=numpy.iinfo(numpy.int64).max,
+                                      end=numpy.iinfo(numpy.int64).min,
+                                      sample_rate=sr)
     for start, end in tqdm(intervals, desc=f"Writing {input_path}"):
-        for sub_start in range(start, end, int(sr * max_length)):
-            sub_end = min(sub_start + int(sr * max_length), end)
-            audio_cut = audio[sub_start:sub_end]
-            sf.write(
-                (
-                    output_dir
-                    / f"{input_path.stem}_{sub_start / sr:.3f}_{sub_end / sr:.3f}.wav"
-                ),
-                audio_cut,
-                sr,
-            )
+        audio_seg = AudioSegment(start, end, sr)
+        new_composed_audio_seg = composed_audio_seg + audio_seg
+        # the 5s - 15s slice is suggested by:
+        # https://github.com/svc-develop-team/so-vits-svc#0-slice-audio
+        if new_composed_audio_seg.len() <= 15.0:
+            composed_audio_seg = new_composed_audio_seg
+        else:
+            if 5.0 <= composed_audio_seg.len() and composed_audio_seg.len() <= 15.0:
+                composed_audio_seg.save(audio, output_dir, input_path.stem)
+                dur_t_sum += composed_audio_seg.len()
+            composed_audio_seg = audio_seg
+    return dur_t_sum
 
 
 def preprocess_split(
@@ -63,8 +128,9 @@ def preprocess_split(
     output_dir = Path(output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
     input_paths = list(input_dir.rglob("*.*"))
+    dur_t_sums = []
     with tqdm_joblib(desc="Splitting", total=len(input_paths)):
-        Parallel(n_jobs=n_jobs)(
+        dur_t_sums = Parallel(n_jobs=n_jobs)(
             delayed(_process_one)(
                 input_path,
                 output_dir / input_path.relative_to(input_dir).parent,
@@ -76,3 +142,4 @@ def preprocess_split(
             )
             for input_path in input_paths
         )
+    LOG.info(f"total valid duration (in seconds): {math.fsum(dur_t_sums):.3f}")
